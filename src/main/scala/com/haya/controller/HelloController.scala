fbc0873545fe1922279cc4f0433247dc1e227804
hayaking <1028779917@qq.com>
2020-04-09 13:46:06 +0800

    Initial commit

---
+++ src/main/scala/com/haya/controller/HelloController.scala
@@ -0,0 +1,12 @@
+package com.haya.controller
+
+import org.springframework.web.bind.annotation.{GetMapping, RestController}
+
+@RestController
+class HelloController {
+
+  @GetMapping(value = Array("hello"))
+  def hello(): String = {
+    return "hello,scala"
+  }
+}
